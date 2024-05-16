774b9e4c1bfb5742c3b27e1c17505c17741df3b4
jtabibito <jtabibito@qq.com>
2024-05-16 15:59:44 +0800

    feat(Mesh): update benchmark case of mesh

--- src/galacean/mesh.ts
+++ src/galacean/mesh.ts
@@ -6,19 +6,13 @@
 import {
   Camera,
   MeshRenderer,
-  MeshTopology,
   PrimitiveMesh,
   WebGLEngine,
   AssetType,
   UnlitMaterial,
   Texture2D,
-  VertexElement
 } from '@galacean/engine';
-import {
-  Stats,
-  OrbitControl
-} from "@galacean/engine-toolkit";
-import * as dat from "dat.gui";
+import { OrbitControl } from "@galacean/engine-toolkit";
 
 function segmentsValue(triangles: number) {
   return Math.floor(Math.sqrt(triangles / 2));
@@ -33,7 +27,6 @@ WebGLEngine.create({canvas: "canvas"}).then((engine) => {
   const cameraEntity = scene.createRootEntity("Camera");
   cameraEntity.transform.setPosition(0, 0, 10);
   cameraEntity.addComponent(Camera);
-  cameraEntity.addComponent(Stats);
   cameraEntity.addComponent(OrbitControl);
 
   engine.resourceManager.load(
@@ -45,36 +38,10 @@ WebGLEngine.create({canvas: "canvas"}).then((engine) => {
     const modelEntity = scene.createRootEntity("Model");
 
     const mr = modelEntity.addComponent(MeshRenderer);
-    mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(8), true);
+    mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(4000000), true);
     const mat = new UnlitMaterial(engine);
     mr.setMaterial(mat);
     mat.baseTexture = texture as Texture2D;
-
-    const params = {
-      Tris1e2: function () {
-        mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(100), false);
-      },
-      Tris1e3: function () {
-        mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(1e3), false);
-      },
-      Tris1e4: function () {
-        mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(1e4), false);
-      },
-      Tris1e7: function () {
-        mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(1e7), false);
-      },
-      Tris2e7: function () {
-        mr.mesh = PrimitiveMesh.createSphere(engine, 1, segmentsValue(2e7), false);
-      },
-      topology: MeshTopology.Triangles,
-    };
-
-    const gui = new dat.GUI();
-    gui.add(params, "Tris1e2").name("100面");
-    gui.add(params, "Tris1e3").name("1000面");
-    gui.add(params, "Tris1e4").name("10,000面");
-    gui.add(params, "Tris1e7").name("10,000,000面");
-    gui.add(params, "Tris2e7").name("20,000,000面");
   });
 
   engine.run();
