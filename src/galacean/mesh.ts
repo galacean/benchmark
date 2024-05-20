fe7a57c87229eb9ae9b5151da50cb7669daa2261
jtabibito <jtabibito@qq.com>
2024-05-20 15:45:00 +0800

    feat(mesh): update benchmark case of mesh

--- src/galacean/mesh.ts
+++ src/galacean/mesh.ts
@@ -31,35 +31,51 @@ WebGLEngine.create({canvas: "canvas"}).then((engine) => {
 
   const root = scene.createRootEntity("root");
 
-  function createModel(url) {
+  const border = 50;
+  function createModel(url, instantiateCount) {
     if (root.children.length > 0) {
       const children = root.children;
-      for (let i = 0, count = children.length; i < count; i++) {
+      for (let i = children.length - 1; i >= 0; i--) {
         children[i].destroy();
       }
     }
     engine.resourceManager.load(url).then((resource) => {
       const gltf = <GLTFResource>resource;
       const model = gltf.instantiateSceneRoot();
-      model.transform.scale.scale(0.3);
+      model.transform.scale.scale(0.03);
       root.addChild(model);
+      for (let i = 0, length = instantiateCount - 1; i < length; ++i) {
+        const entity = model.clone();
+        const x = Math.random() * border - border / 2;
+        const z =  Math.random() * border - border / 2;
+        const transform = entity.transform;
+        transform.setPosition(x, 0, -z);
+        transform.setRotation(-90, Math.random() * 360, 0);
+        root.addChild(entity);
+      }
     });
   }
 
   const params = {
-    model: "Base"
+    model: "Low",
+    instantiated: 1
   };
   const modelURL = {
     "Low": "https://mdn.alipayobjects.com/oasis_be/afts/file/A*m9BeQLj1NCEAAAAAAAAAAAAADkp5AQ/popcat_combine_low.glb",
-    "Base": "https://mdn.alipayobjects.com/oasis_be/afts/file/A*WmviTKgN7_QAAAAAAAAAAAAADkp5AQ/popcat_combine.glb",
+    "Medium": "https://mdn.alipayobjects.com/oasis_be/afts/file/A*WmviTKgN7_QAAAAAAAAAAAAADkp5AQ/popcat_combine.glb",
     "High": "https://mdn.alipayobjects.com/oasis_be/afts/file/A*r7NhSKXOH6gAAAAAAAAAAAAADkp5AQ/popcat_combine_high2.glb"
   }
   const gui = new dat.GUI();
   gui.add(params, "model", Object.keys(modelURL)).onChange(value => {
-    createModel(modelURL[value]);
+    createModel(modelURL[value], 1);
+    params.instantiated = 1;
+    gui.updateDisplay();
+  });
+  gui.add(params, "instantiated", 1, 5000, 1).onFinishChange(value => {
+    createModel(modelURL[params.model], value);
   });
 
-  createModel(modelURL[params.model]);
+  createModel(modelURL[params.model], 1);
 
   engine.run();
 });
