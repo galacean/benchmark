f5fc26c82e964beb94e3208d89f07f4cb30e06c0
gz65555 <gz65555@gmail.com>
2024-05-22 17:03:30 +0800

    refactor: rendering

--- src/galacean/point-light.ts
+++ src/galacean/point-light.ts
@@ -11,55 +11,53 @@ import {
   PointLight,
   PrimitiveMesh,
   UnlitMaterial,
-  WebGLEngine
+  WebGLEngine,
 } from "@galacean/engine";
 import { OrbitControl } from "@galacean/engine-toolkit";
 Logger.enable();
 
-function random(min: number, max: number) {
-  return Math.random() * (max - min) + min;
-}
+WebGLEngine.create({
+  canvas: "canvas",
+  graphicDeviceOptions: { powerPreference: "high-performance" },
+}).then((engine) => {
+  engine.canvas.resizeByClientSize();
+  engine.sceneManager.activeScene.ambientLight.diffuseIntensity = 0;
 
-WebGLEngine.create({ canvas: "canvas", graphicDeviceOptions: { powerPreference: "high-performance" } }).then(
-  (engine) => {
-    engine.canvas.resizeByClientSize();
+  const rootEntity = engine.sceneManager.activeScene.createRootEntity();
 
-    const rootEntity = engine.sceneManager.activeScene.createRootEntity();
+  const scale = 2.5;
+  const cameraEntity = rootEntity.createChild("camera");
+  const camera = cameraEntity.addComponent(Camera);
+  camera.fieldOfView = 45;
+  cameraEntity.transform.setPosition(6.5 * scale, 6.58 * scale, 8.5 * scale);
+  cameraEntity.addComponent(OrbitControl);
 
-    const cameraEntity = rootEntity.createChild("camera");
-    cameraEntity.addComponent(Camera);
-    cameraEntity.transform.setPosition(0, 6, 40);
-    cameraEntity.addComponent(OrbitControl);
+  const lightCount = 10;
+  for (let i = 0; i < lightCount; i++) {
+    const lightEntity = rootEntity.createChild("light" + i);
+    lightEntity.transform.setPosition(-8 + i * 2, 5, 0);
+    const light = lightEntity.addComponent(PointLight);
+    light.distance = 20;
+    light.color.set(0.5, 0.2, 0.7, 1.0);
 
-    const planeEntity = rootEntity.createChild();
-    const planeRenderer = planeEntity.addComponent(MeshRenderer);
-    const planeMat = new PBRMaterial(engine);
-    planeRenderer.mesh = PrimitiveMesh.createPlane(engine, 1000, 1000);
-    planeRenderer.setMaterial(planeMat);
-
-    const lightCount = 10;
-    for (let i = 0; i < lightCount; i++) {
-      const lightEntity = rootEntity.createChild("light" + i);
-      lightEntity.transform.setPosition(random(-5, 5), random(1, 4), random(-5, 5));
-      const light = lightEntity.addComponent(PointLight);
-      light.distance = 5;
-      light.color.set(random(0, 1), random(0, 1), random(0, 1), 1);
-
-      const lightRenderer = lightEntity.addComponent(MeshRenderer);
-      lightRenderer.mesh = PrimitiveMesh.createSphere(engine, 0.3, 32);
-      lightRenderer.setMaterial(new UnlitMaterial(engine));
-    }
-
-    engine.resourceManager
-      .load<GLTFResource>(
-        "https://mdn.alipayobjects.com/oasis_be/afts/file/A*nceKQadLab8AAAAAAAAAAAAADkp5AQ/DamagedHelmet.glb"
-      )
-      .then((gltf) => {
-        const entity = rootEntity.createChild("");
-        const defaultSceneRoot = gltf.instantiateSceneRoot();
-        defaultSceneRoot.transform.setPosition(0, 1, 0);
-        entity.addChild(defaultSceneRoot);
-      }),
-      engine.run();
+    const lightRenderer = lightEntity.addComponent(MeshRenderer);
+    lightRenderer.mesh = PrimitiveMesh.createSphere(engine, 0.3, 32);
+    lightRenderer.setMaterial(new UnlitMaterial(engine));
   }
-);
+
+  engine.resourceManager
+    .load<GLTFResource>(
+      "https://mdn.alipayobjects.com/oasis_be/afts/file/A*nceKQadLab8AAAAAAAAAAAAADkp5AQ/DamagedHelmet.glb"
+    )
+    .then((gltf) => {
+      const entity = rootEntity.createChild("");
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          const defaultSceneRoot = gltf.instantiateSceneRoot();
+          defaultSceneRoot.transform.setPosition(-8 + i * 2, 2, -10 + j * 2);
+          entity.addChild(defaultSceneRoot);
+        }
+      }
+    }),
+    engine.run();
+});
