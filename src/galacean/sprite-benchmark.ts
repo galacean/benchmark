47bda61149d8ce221a299f12809b95b870beea3e
singlecoder <singlecoder2014@gmail.com>
2024-06-26 10:45:31 +0800

    opt code

--- src/galacean/sprite-benchmark.ts
+++ src/galacean/sprite-benchmark.ts
@@ -8,6 +8,7 @@ import {
   Camera,
   Script,
   Sprite,
+  SpriteAtlas,
   SpriteRenderer,
   Texture2D,
   WebGLEngine,
@@ -35,12 +36,13 @@ WebGLEngine.create({
   camera.orthographicSize = (engine.canvas.height * 0.5) / 100;
 
   engine.resourceManager
-    .load<Texture2D>({
-      url: "https://mdn.alipayobjects.com/huamei_w6ifet/afts/img/A*YwyMRq8_O-4AAAAAAAAAAAAADjCHAQ/original",
-      type: AssetType.Texture2D,
+    .load<SpriteAtlas>({
+      url: "https://mdn.alipayobjects.com/oasis_be/afts/file/A*s_FUSawb0sgAAAAAAAAAAAAADkp5AQ/SpriteAtlas.json",
+      type: AssetType.SpriteAtlas,
     })
-    .then((texture: Texture2D) => {
-      const sprite = new Sprite(engine, texture);
+    .then((atlas: SpriteAtlas) => {
+      const sprite1 = atlas.getSprite("/tex1-spr.png");
+      const sprite2 = atlas.getSprite("/tex2-spr.png");
 
       const col = 88;
       const row = 90;
@@ -55,7 +57,7 @@ WebGLEngine.create({
           const entity = rootEntity.createChild(`sprite-${i}`);
           const sr = entity.addComponent(SpriteRenderer);
           entity.addComponent(Rotate);
-          sr.sprite = sprite;
+          sr.sprite = i % 2 === 0 ? sprite1 : sprite2;
           sr && (sr.priority = index++);
           const transform = entity.transform;
           const scale = 0.075;
--- src/pixi/sprite-benchmark.ts
+++ src/pixi/sprite-benchmark.ts
@@ -15,13 +15,19 @@ app
     height: canvas.height,
   })
   .then(() => {
-    PIXI.Assets.load({
-      src: "https://mdn.alipayobjects.com/huamei_w6ifet/afts/img/A*YwyMRq8_O-4AAAAAAAAAAAAADjCHAQ/original",
-      loadParser: "loadTextures",
-    }).then((texture) => {
+    PIXI.Assets.load([
+      {
+        src: "https://mdn.alipayobjects.com/huamei_w6ifet/afts/img/A*YwyMRq8_O-4AAAAAAAAAAAAADjCHAQ/original",
+        loadParser: "loadTextures",
+      },
+      {
+        src: "https://mdn.alipayobjects.com/huamei_w6ifet/afts/img/A*ClJLS6U4HtsAAAAAAAAAAAAADjCHAQ/original",
+        loadParser: "loadTextures",
+      },
+    ]).then((textureRecord) => {
       const col = 88;
       const row = 90;
-      const offsetX = 4.25;
+      const offsetX = 5;
       const offsetY = 10;
       // 中心点所在行列
       const centerX = col * 0.5;
@@ -31,13 +37,18 @@ app
       const halfWidth = screen.width * 0.5;
       const halfHeight = screen.height * 0.5;
       const sprites = [];
+      const texture1 =
+        textureRecord[
+          "https://mdn.alipayobjects.com/huamei_w6ifet/afts/img/A*YwyMRq8_O-4AAAAAAAAAAAAADjCHAQ/original"
+        ];
+      const texture2 = textureRecord['https://mdn.alipayobjects.com/huamei_w6ifet/afts/img/A*ClJLS6U4HtsAAAAAAAAAAAAADjCHAQ/original'];
       for (let i = 0; i < row; ++i) {
         for (let j = 0; j < col; ++j) {
-          const sprite = new PIXI.Sprite(texture);
+          const sprite = new PIXI.Sprite(i % 2 === 0 ? texture1 : texture2);
           app.stage.addChild(sprite);
           sprite.scale = 0.045;
           sprite.anchor.set(0.5, 0.5);
-          sprite.x = halfWidth + (j - centerX) * offsetX + 10;
+          sprite.x = halfWidth + (j - centerX) * offsetX + 5;
           sprite.y = halfHeight + (i - centerY) * offsetY + 10;
           sprites.push(sprite);
         }
