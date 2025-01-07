27dda2ff90cadbe09ee09e022d32baca1cad5e37
木鳐 <zhubei.zb@antgroup.com>
2025-01-07 11:23:32 +0800

    feat: delete

--- src/pixi/spine-benchmark.ts
+++ src/pixi/spine-benchmark.ts
@@ -16,38 +16,25 @@ app
     height: canvas.height,
   })
   .then(() => {
-    
-    // Pre-load the skeleton data and atlas. You can also load .json skeleton data.
     PIXI.Assets.add({alias: "spineboyData", src: "/spineboy.json"});
     PIXI.Assets.add({alias: "spineboyAtlas", src: "/spineboy.atlas"});
     PIXI.Assets.load(["spineboyData", "spineboyAtlas"]).then(() => {
-
-      // Create the spine display object
       const spineboy = Spine.from({skeleton: "spineboyData", atlas: "spineboyAtlas",
         scale: 0.5,
       });
+      spineboy.state.setAnimation(0, "run", true);
+      spineboy.x = window.innerWidth / 2;
+      spineboy.y = window.innerHeight / 2 + spineboy.getBounds().height / 2;
 
-      // Set the default mix time to use when transitioning
-        // from one animation to the next.
-
-        // Set animation "run" on track 0, looped.
-        spineboy.state.setAnimation(0, "run", true);
-
-        // Center the spine object on screen.
-        spineboy.x = window.innerWidth / 2;
-        spineboy.y = window.innerHeight / 2 + spineboy.getBounds().height / 2;
-
-        // Clone spineboy in a row
-        const clones = 300; // Number of clones
-        for (let i = 1; i < clones; i++) {
-          const clone = Spine.from({skeleton: "spineboyData", atlas: "spineboyAtlas",
-            scale: 0.8,
-          });
-          clone.state.setAnimation(0, "walk", true); // Set animation for each clone
-          clone.x = -180 + spineboy.x + i * 1.5; // Horizontal spacing between clones
-          clone.y = spineboy.y;
-          app.stage.addChild(clone);
-        }
-
+      const clones = 300; // Number of clones
+      for (let i = 1; i < clones; i++) {
+        const clone = Spine.from({skeleton: "spineboyData", atlas: "spineboyAtlas",
+          scale: 0.8,
+        });
+        clone.state.setAnimation(0, "walk", true);
+        clone.x = -180 + spineboy.x + i * 1.5;
+        clone.y = spineboy.y;
+        app.stage.addChild(clone);
+      }
     });
   });
